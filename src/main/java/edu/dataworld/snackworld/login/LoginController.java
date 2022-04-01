ab4367c8b63bf6d11cacc3e0af1d6f9c1b403c05
jangsangik <rkd0812@gmail.com>
2022-04-01 16:01:16 +0900

    tiles template 설정 추가 - pom.xml dependency 추가 - tiles-layout 설정 - tiles-define.xml 설정 - 테스트를 위한 로그인 페이지 추가

---
+++ src/main/java/edu/dataworld/snackworld/login/LoginController.java
@@ -0,0 +1,16 @@
+package edu.dataworld.snackworld.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/login")
+public class LoginController {
+
+    @RequestMapping("/loginForm.do")
+    public String loginForm() {
+        return "login/loginForm";
+    }
+
+
+}
--- src/main/java/edu/dataworld/snackworld/main/web/MainController.java
+++ src/main/java/edu/dataworld/snackworld/main/web/MainController.java
@@ -1,4 +1,14 @@
 package edu.dataworld.snackworld.main.web;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+
+    @RequestMapping(value="/index.do")
+    public String index() {
+        return "index";
+    }
 }
